6467a5f0878c23a945fcf0c3259ae7a949cafb5d
Sushma92 <nuthanap1992@gmail.com>
2019-11-30 01:48:02 -0600

    Implemented Backend

---
+++ sushma.java.userdata/src/main/java/com/sushma/java/userdata/User.java
@@ -0,0 +1,43 @@
+package com.sushma.java.userdata;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private String email;
+	
+	public User() {
+		this.name = "";
+		this.email = "";
+	}
+	
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	@Override
+	public String toString() {
+		return "User{" + "id=" + ", name=" + name + ", email" + email +"}";
+	}
+}
---
+++ sushma.java.userdata/src/main/java/com/sushma/java/userdata/UserController.java
@@ -0,0 +1,30 @@
+package com.sushma.java.userdata;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+	private final UserRepository userRepository;
+	
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	@GetMapping("/Users")
+	public List<User> getUsers(){
+		return (List<User>) userRepository.findAll();
+	}
+	
+	@PostMapping("/Users")
+	void addUser(@RequestBody User user) {
+		userRepository.save(user);
+	}
+}
